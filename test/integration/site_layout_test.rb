13251a46f677c08ef15cd2b485dd3312ea9d449b
Zachary R. Smith <ZacharyRSmith@gmail.com>
2015-07-29 01:47:10 +0000

    Update test for layout links

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,12 +2,41 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
-  test "layout links" do
+  def setup
+    @user = users(:michael)
+  end
+
+  test "layout links' presence when logged in" do
+    log_in_as(@user)
     get root_path
     assert_template 'static_pages/home'
+
+    # Header
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
+
+    # Footer
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", "http://news.railstutorial.org/"
+  end
+
+  test "layout links' presence when logged out" do
+    get root_path
+    assert_template 'static_pages/home'
+
+    # Header
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", login_path
+
+    # Footer
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", "http://news.railstutorial.org/"
   end
 end
