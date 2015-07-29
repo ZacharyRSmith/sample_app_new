77cba170601ec1d9ca864cfb17cffc85b0cb73a0
Zachary R. Smith <ZacharyRSmith@gmail.com>
2015-07-29 01:34:52 +0000

    Finish User REST actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :index, :update]
+  before_action :logged_in_user, only: [:destroy, :edit, :index, :update]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
 
   def create
     @user = User.new(user_params)
@@ -12,6 +13,12 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
   end
 
   def edit
@@ -39,6 +46,13 @@ class UsersController < ApplicationController
   end
 
   private
+
+    # Confirms current user is admin
+    def admin_user
+      if !current_user.admin?
+        redirect_to(root_url)
+      end
+    end
 
     # Confirms the correct user
     def correct_user
